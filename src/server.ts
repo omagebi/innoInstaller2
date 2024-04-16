c500fc772746937c8451339e5e8a81d6234e8e08
Femi Omage <omagebi@yahoo.com>
2024-04-16 14:20:46 +0100

    hello

---
+++ src/server.ts
@@ -0,0 +1,119 @@
+/**
+ * Setup express server.
+ */
+
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import path from 'path';
+import helmet from 'helmet';
+import express, { Request, Response, NextFunction } from 'express';
+import logger from 'jet-logger';
+
+import 'express-async-errors';
+
+import BaseRouter from '@src/routes/api';
+import Paths from '@src/constants/Paths';
+
+import EnvVars from '@src/constants/EnvVars';
+import HttpStatusCodes from '@src/constants/HttpStatusCodes';
+
+import { NodeEnvs } from '@src/constants/misc';
+import { RouteError } from '@src/other/classes';
+
+import fs   from 'fs';
+
+// **** Variables **** //
+
+const app = express();
+
+
+// **** Setup **** //
+
+// Basic middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser(EnvVars.CookieProps.Secret));
+
+// Show routes called in console during development
+if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
+  app.use(morgan('dev'));
+}
+
+// Security
+if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
+  app.use(helmet());
+}
+
+// Add APIs, must be after middleware
+app.use(Paths.Base, BaseRouter);
+
+// Add error handler
+app.use((
+  err: Error,
+  _: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
+) => {
+  if (EnvVars.NodeEnv !== NodeEnvs.Test.valueOf()) {
+    logger.err(err, true);
+  }
+  let status = HttpStatusCodes.BAD_REQUEST;
+  if (err instanceof RouteError) {
+    status = err.status;
+  }
+  return res.status(status).json({ error: err.message });
+});
+
+
+// ** Front-End Content ** //
+
+// Set views directory (html)
+const viewsDir = path.join(__dirname, 'views');
+app.set('views', viewsDir);
+
+// Set static directory (js and css).
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
+
+// Nav to home pg by default
+app.get('/', (_: Request, res: Response) => {
+  return res.sendFile('home.html', { root: viewsDir });
+});
+
+app.get('/download', (req, res) => {
+  const username = req.query.username; // Get the username from the query parameter
+  // Assuming installer file is located in the same directory named innoInstaller.exe
+  const filePath = `${__dirname}/innoInstaller.exe`;
+
+  // Check if the file exists
+  fs.exists(filePath, (exists) => {
+    if (exists) {
+      // Set the appropriate headers for the response
+      res.setHeader('Content-disposition', `attachment; filename=innoInstaller.exe`);
+      res.setHeader('Content-type', 'application/octet-stream');
+
+      // Read the file and send it as the response
+      const fileStream = fs.createReadStream(filePath);
+      fileStream.pipe(res);
+    } else {
+      // If the file does not exist, send a 404 error
+      res.status(404).send('File not found');
+    }
+  });
+});
+
+// Nav to users pg by default
+// app.get('/', (_: Request, res: Response) => {
+//   return res.redirect('/users');
+// });
+
+// Redirect to login if not logged in.
+app.get('/users', (_: Request, res: Response) => {
+  return res.sendFile('users.html', { root: viewsDir });
+});
+
+
+// **** Export default **** //
+
+export default app;
